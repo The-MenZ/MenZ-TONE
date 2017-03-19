49d8e0517c2420aeb706b4238d0e8c2261a1de09
zagan-the-gun <zagan.the.gun@gmail.com>
2017-03-19 18:52:18 +0900

    7セグの点灯モードを変えて省電力&発熱防止

--- MenZ-TONE.ino
+++ MenZ-TONE.ino
@@ -5,17 +5,20 @@
 
 const int PowerLedPin = 13;      // the number of the LED pin
 const int ledPin = 2;      // the number of the LED pin
-
-const int seg1Pin = 9;      // the number of the LED pin
-const int seg2Pin = 10;      // the number of the LED pin
-const int seg4Pin = 11;      // the number of the LED pin
-const int seg5Pin = 12;      // the number of the LED pin x
-const int seg6Pin = 5;      // the number of the LED pin
-const int seg7Pin = 6;      // the number of the LED pin
-const int seg9Pin = 7;      // the number of the LED pin
-const int seg10Pin = 8;      // the number of the LED pin
 const int anode_pins[] = {9, 10, 11, 5, 6, 7, 8};    // アノードに接続するArduinoのピン
-int melodyNo = 0;
+//const int digits[] = {
+volatile int digits[] = {
+  0b01000000, // 0
+  0b01110011, // 1
+  0b00100100, // 2
+  0b00100001, // 3
+  0b00010011, // 4
+  0b00001001, // 5
+  0b00001000, // 6
+  0b01000011, // 7
+  0b00000000, // 8
+  0b00000001, // 9
+};
 
 const int buttonPin = 3;    // the number of the pushbutton pin
 int buttonState = 0;             // the current reading from the input pin
@@ -27,7 +30,7 @@ int lastUpButtonState = LOW;             // the current reading from the input p
 long upButtonDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
-int currentSong = 0;
+volatile int currentSong = 0;
 int currentPosition = 0;
 int readyTone = 1;
 int nowNote;
@@ -35,24 +38,34 @@ int nowNote;
 void ledBlink() {
   static boolean output = LOW;  // プログラム起動前に１回だけHIGH(1)で初期化される
   digitalWrite(PowerLedPin, output);      // 13番ピン(LED)に出力する(HIGH>ON LOW>OFF)
+  if(output){
+    for (int i = 0; i < (sizeof(anode_pins) / sizeof(anode_pins[0])); i++) {
+      digitalWrite(anode_pins[i], digits[currentSong] & (1 << i) ? HIGH : LOW);
+    }
+  } else {
+    for (int i = 0; i < (sizeof(anode_pins) / sizeof(anode_pins[0])); i++) {
+      digitalWrite(anode_pins[i], HIGH);
+    }
+  }
   output = !output;              // 現在のoutput内容を反転(HIGH→LOW/LOW→HIGH)させoutputにセットする
 }
 
 void setup() {
   pinMode(PowerLedPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
-  pinMode(seg1Pin, OUTPUT);
-  pinMode(seg2Pin, OUTPUT);
-  pinMode(seg4Pin, OUTPUT);
-  pinMode(seg5Pin, OUTPUT);
-  pinMode(seg6Pin, OUTPUT);
-  pinMode(seg7Pin, OUTPUT);
-  pinMode(seg9Pin, OUTPUT);
-  pinMode(seg10Pin, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
   PORTD = B11100000;
 
 //  digitalWrite(PowerLedPin, HIGH);
-  Timer1.initialize(1000000); //マイクロ秒単位で設定
+//  Timer1.initialize(1000000); //マイクロ秒単位で設定
+  Timer1.initialize(15000); //マイクロ秒単位で設定
   Timer1.attachInterrupt(ledBlink);
 
   pinMode(buttonPin, INPUT);
@@ -80,37 +93,6 @@ void loop() {
         currentSong++;
         if(currentSong >= songNum) currentSong = 0;
       }
-    }
-  }
-
-const int digits[] = {
-  0b01000000, // 0
-  0b01110011, // 1
-  0b00100100, // 2
-  0b00100001, // 3
-  0b00010011, // 4
-  0b00001001, // 5
-  0b00001000, // 6
-  0b01000011, // 7
-  0b00000000, // 8
-  0b00000001, // 9
-};
-
-  if(melodyNo == 0){
-    for (int i = 0; i < (sizeof(anode_pins) / sizeof(anode_pins[0])); i++) {
-      digitalWrite(anode_pins[i], digits[1] & (1 << i) ? HIGH : LOW);
-    }
-  } else if(melodyNo == 1){
-    for (int i = 0; i < (sizeof(anode_pins) / sizeof(anode_pins[0])); i++) {
-      digitalWrite(anode_pins[i], digits[8] & (1 << i) ? HIGH : LOW);
-    }
-  } else if(melodyNo == 2){
-    for (int i = 0; i < (sizeof(anode_pins) / sizeof(anode_pins[0])); i++) {
-      digitalWrite(anode_pins[i], digits[9] & (1 << i) ? HIGH : LOW);
-    }
-  } else if(melodyNo == 3){
-    for (int i = 0; i < (sizeof(anode_pins) / sizeof(anode_pins[0])); i++) {
-      digitalWrite(anode_pins[i], digits[melodyNo] & (1 << i) ? HIGH : LOW);
     }
   }
 
